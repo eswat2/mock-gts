2d187355c4bbe795dec3919324e504051568ab1b
Richard Hess <eswat2@gmail.com>
2022-02-13 11:48:39 -0800

    add vins api

--- api/greetings.js
+++ api/greetings.js
@@ -2,6 +2,6 @@ module.exports = (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.json({
     message: 'hooray! welcome to our api server!...',
-    apis: ['cars', 'colors', 'exotics', 'groups', 'makes', 'solution', 'stats'],
+    apis: ['cars', 'colors', 'exotics', 'groups', 'makes', 'solution', 'stats', 'vins'],
   })
 }
